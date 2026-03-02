7bdb170b4c7d1ceda6c94a775576b19362b97016
JuanManuelBorrego <88849486+JuanManuelBorrego@users.noreply.github.com>
2026-03-02 11:40:12 -0300

    Update db.py

--- db.py
+++ db.py
@@ -1,1 +1,23 @@
+import os
+from libsql_client import create_client
 
+# Cliente global
+client = create_client(
+    url=os.environ["TURSO_DATABASE_URL"],
+    auth_token=os.environ["TURSO_AUTH_TOKEN"]
+)
+
+def query(sql, params=None):
+    if params:
+        result = client.execute(sql, params)
+    else:
+        result = client.execute(sql)
+
+    return result.rows
+
+
+def execute(sql, params=None):
+    if params:
+        client.execute(sql, params)
+    else:
+        client.execute(sql)
