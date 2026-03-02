ec9890dbc3227ee1fd7e792a776b43f4d191488d
JuanManuelBorrego <88849486+JuanManuelBorrego@users.noreply.github.com>
2026-03-02 18:25:39 -0300

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,11 @@
 import libsql
-import os
 
 def conectar():
-    # Esta librería es la recomendada para evitar errores de OSError en Streamlit
-    conn = libsql.connect(
-        database="https://astradb-juanmanuelborrego.aws-us-east-1.turso.io",
-        auth_token="eyJhbGciOiJFZERTQSIsInR5cCI6IkpXVCJ9.eyJhIjoicnciLCJpYXQiOjE3NzI0NjA5MTUsImlkIjoiMDE5Y2FlZTItZDgwMS03ZTg0LWFhNzAtOGFmMGE4YTYwMzk3IiwicmlkIjoiMWQ2NmE1ZDYtZGE1Mi00Y2VjLWI0MGEtMjg0ZDk3YWQ3YjFlIn0.rrSrgrFcgfFI4gw2tCyWBxIU0wv-PQ9RA5DBxsQjs3HLHXyg-yztr3isa5-pXiRxr_cy-E6hdohFfzPlx7t9Cw"
-    )
+    # Usar build_client es lo que garantiza que el objeto tenga .execute()
+    url = "https://astradb-juanmanuelborrego.aws-us-east-1.turso.io"
+    token = "eyJhbGciOiJFZERTQSIsInR5cCI6IkpXVCJ9.eyJhIjoicnciLCJpYXQiOjE3NzI0NjA5MTUsImlkIjoiMDE5Y2FlZTItZDgwMS03ZTg0LWFhNzAtOGFmMGE4YTYwMzk3IiwicmlkIjoiMWQ2NmE1ZDYtZGE1Mi00Y2VjLWI0MGEtMjg0ZDk3YWQ3YjFlIn0.rrSrgrFcgfFI4gw2tCyWBxIU0wv-PQ9RA5DBxsQjs3HLHXyg-yztr3isa5-pXiRxr_cy-E6hdohFfzPlx7t9Cw"
     
+    return libsql.build_client(url, auth_token=token)
 
 ruta = "ad_astra.db"
+
