9b67ecb266b78b5cafc1ff6888a3bb667b11b25b
JuanManuelBorrego <88849486+JuanManuelBorrego@users.noreply.github.com>
2026-03-02 17:58:53 -0300

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ import os
 def conectar():
     # USAR create_client_sync es la clave para que exista el método .cursor()
     client = libsql_client.create_client_sync(
-        url="https://astradb-juanmanuelborrego.aws-us-east-1.turso.io", 
+        url="libsql://astradb-juanmanuelborrego.aws-us-east-1.turso.io", 
         auth_token="eyJhbGciOiJFZERTQSIsInR5cCI6IkpXVCJ9.eyJhIjoicnciLCJpYXQiOjE3NzI0NjA5MTUsImlkIjoiMDE5Y2FlZTItZDgwMS03ZTg0LWFhNzAtOGFmMGE4YTYwMzk3IiwicmlkIjoiMWQ2NmE1ZDYtZGE1Mi00Y2VjLWI0MGEtMjg0ZDk3YWQ3YjFlIn0.rrSrgrFcgfFI4gw2tCyWBxIU0wv-PQ9RA5DBxsQjs3HLHXyg-yztr3isa5-pXiRxr_cy-E6hdohFfzPlx7t9Cw"
     )
     # --- TRUCO DE COMPATIBILIDAD ---
