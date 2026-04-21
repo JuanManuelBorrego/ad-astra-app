63922b49354bdc1b908e323a62ca12913ce979d4
JuanManuelBorrego <88849486+JuanManuelBorrego@users.noreply.github.com>
2026-04-21 16:44:44 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -333,15 +333,15 @@ if modo == "Estudiantes":
                                     
                                     elif p_actual <= 5:
                                         st.subheader(f"✨ #{p_actual}")
-                                        st.success("¡En el Cuadro!")
+                                        st.success("¡En el Cuadro de Honor del curso!")
                                     
                                     elif p_actual <= 10:
                                         st.subheader(f"⚡ #{p_actual}")
-                                        st.info("¡Casi entrás!")
+                                        st.info(" Estás en el TOP 10: ¡estás a nada de entrar en el Cuadro de Honor del curso!")
                                     
                                     else:
                                         st.subheader(f"🚀 #{p_actual}")
-                                        st.warning("¡A seguir sumando!")
+                                        st.warning("¡A seguir sumando para llegar a estar en el TOP 10!")
                                     
                                     st.markdown("</div>", unsafe_allow_html=True)
                     else:
